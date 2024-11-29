57cdff5f08807d46ade30d1cd7cbc97ae49074a8
Khashkhuu Batchoij <khashkhuu.b@cumongol.mn>
2024-11-29 10:24:50 +0800

    Initial

---
+++ engine/config/connector.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2')
+
+const connector = mysql.createConnection({
+    user: process.env.MYSQL_USER,
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD
+})
+
+connector.connect((err) => {
+    if (err) {
+        console.error('MYSQL connection error: ', err)
+    } else {
+        console.log('MYSQL connected successfully')
+    }
+})
+
+module.exports = { connector }
---
+++ engine/controller/lottery.controller.js
@@ -0,0 +1,58 @@
+const util = require('util')
+const { connector } = require('../config/connector')
+const query = util.promisify(connector.query).bind(connector)
+
+module.exports = {
+
+    checkLotteryNumber: async (yw, ir) => {
+
+        try {
+
+            const { store_number, transaction_number, amount } = yw.body
+
+            if (!store_number || !transaction_number || !amount) {
+                return res.status(400).json({ response: 'Мэдээлэл буруу байна' });
+            }
+
+            const results = await query(
+                "SELECT * FROM ebarimt.lottery WHERE store_number = ? AND transaction_number = ? AND amount = ? LIMIT 1",
+                [store_number, transaction_number, amount]
+            )
+
+            if (results.length === 0) {
+                return ir.status(404).json({ response: 'Мэдээлэл олдсонгүй' });
+            }
+
+            return ir.status(200).json({ response: results })
+        }
+        catch (error) {
+            ir.status(500).json({ response: error })
+        }
+    },
+
+    insertLotteryNumber: async (yw, ir) => {
+
+        try {
+
+            const { id, lottery_number } = yw.body
+
+            if (!id || !lottery_number) {
+                return res.status(400).json({ response: 'Мэдээлэл буруу байна' });
+            }
+
+            const results = await query(
+                "UPDATE ebarimt.lottery SET lottery_number = ? WHERE id = ?",
+                [lottery_number, id]
+            )
+
+            if (results.affectedRows === 0) {
+                return ir.status(404).json({ response: 'Мэдээлэл олдсонгүй' });
+            }
+
+            return ir.status(200).json({ response: results })
+        }
+        catch (error) {
+            ir.status(500).json({ response: error })
+        }
+    }
+}
